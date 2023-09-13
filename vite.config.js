82fabc5038384767d7d7148b66825659f96b82ce
ElegantDevil <18150850030@163.com>
2023-09-13 14:30:07 +0800

    feat: 引入模型或资源采用相对路径，否则打包会出现资源找不到的问题

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const IS_BUILD = (process.env.NODE_ENV || "").includes("production")
+  ? true
+  : false;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base: IS_BUILD ? "/3dRotateYControl/" : "/",
+  plugins: [vue()],
+  build: {
+    minify: "terser",
+    outDir: "docs/",
+    terserOptions: {
+      compress: {
+        drop_console: IS_BUILD,
+        drop_debugger: IS_BUILD,
+      },
+    }, //去掉console
+  },
+  treeshake: true, // 开启树摇
+  chunkSizeWarningLimit: 1000, // 代码块大小警告的限制调整为1MB
+});
